6dad38e7d1c23fda843d3f2a1b1fb9a1242f250b
Dragos Suciu <dragos.suciu.15@alumni.ucl.ac.uk>
2020-12-07 17:52:53 +0200

    Setting up NavBar, Header and Footer

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as NavBar } from './navBar';
+export { default as apiFrom } from './apiForm';
+export { default as playerForm } from './playerForm';
+export { default as winner } from './winner';
+export { default as quiz } from './quiz';
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import { Provider } from 'react-redux';
-
+import { BrowserRouter as Router} from 'react-router-dom';
 import store from './store';
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <Router>
+                <App />
+            </Router>
         </Provider>
     </React.StrictMode>, 
     document.getElementById('root')
