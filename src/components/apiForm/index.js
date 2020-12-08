de99f81dd42fbd6c4d2e262d92c7751976136902
PaolaDM <orchidea.preziosa@gmail.com>
2020-12-08 10:55:34 +0000

    add form in game page

--- src/components/apiForm/index.js
+++ src/components/apiForm/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getResult } from '../../actions';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
 
 class ApiForm extends React.Component{
     render() {
@@ -8,6 +10,46 @@ class ApiForm extends React.Component{
             <>
                 <h1>{this.props.loading ? "loading" : "error"}</h1>
                 <button onClick={this.handler}>change to true</button>
+                
+                <Form>
+                        
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Label>Select your number of questions</Form.Label>
+                            <Form.Control as="select">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Label>Choose your favourite topic</Form.Label>
+                            <Form.Control as="select">
+                            <option>general knowledge 9</option>
+                            <option>science computers 18</option>
+                            <option>animals 27</option>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Label>Set your difficulty</Form.Label>
+                            <Form.Control as="select">
+                            <option>easy</option>
+                            <option>medium</option>
+                            <option>hard</option>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Label>Select type</Form.Label>
+                            <Form.Control as="select">
+                            <option>True / False</option>
+                            <option>multiple</option>
+                            
+                            </Form.Control>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">Submit </Button>
+                </Form>
+
             </>
         );
     }
