1f40f3f0a15f87b2f005868153f220a89bf94035
Geraldine Bengsch <geraldine_be@hotmail.com>
2020-12-09 17:37:28 +0000

    home page styling, add consistency across pages for header and footer

--- src/layout/footer/index.js
+++ src/layout/footer/index.js
@@ -3,9 +3,8 @@ import './style.css'
 
 const Footer = () => {
     return (
-        <div className="footer">
-        <span id="footer">&copy; Trivia Planet 2020</span>
-        <span> Developed by Raj, Paola, Geraldine, Charan and Dragos</span>
+        <div className="footerContainer">
+            <p id="footer">&copy; Trivia Planet 2020 | Developed by Raj, Paola, Geraldine, Charan and Dragos</p>
         </div>
     );
 }
